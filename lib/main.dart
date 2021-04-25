e50f4ba0450e9f92d38bb1a23c2d3b25683e45a8
Dylan Aron <45520829+DylanNAron@users.noreply.github.com>
2021-04-25 16:16:50 -0400

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,68 @@
+import 'package:flutter/material.dart';
+import 'education.dart';
+import 'meal.dart';
+import 'account.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: MyHomePage(title: 'Tab Bar Demo'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key key, this.title}) : super(key: key);
+
+  // This widget is the home page of your application. It is stateful, meaning
+  // that it has a State object (defined below) that contains fields that affect
+  // how it looks.
+
+  // This class is the configuration for the state. It holds the values (in this
+  // case the title) provided by the parent (in this case the App widget) and
+  // used by the build method of the State. Fields in a Widget subclass are
+  // always marked "final".
+
+  final String title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return DefaultTabController(
+        length: 3,
+        child: Scaffold(
+          appBar: AppBar(
+            title: Text(widget.title),
+            bottom: TabBar(
+              tabs: [
+                Tab(icon: Icon(Icons.person)),
+                Tab(icon: Icon(Icons.food_bank)),
+                Tab(icon: Icon(Icons.lightbulb)),
+              ],
+            ),
+          ),
+          body: TabBarView(
+            children: [
+              AccountPage(),
+              MealPage(),
+              EducationPage(),
+            ],
+          ),
+        ));
+  }
+}
---
+++ lib/meal.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+class MealPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Center(child: Icon(Icons.food_bank, size: 200));
+  }
+}
